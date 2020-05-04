139cf945f9d7db7e63556cdeab2e4851b0f36407
Kapil <kshuklaprof@gmail.com>
2020-05-04 22:29:45 +0000

    Committing the changes with security group configuration into the main.tf and variable pertaining to that

--- main.tf
+++ main.tf
@@ -219,24 +219,137 @@ resource "aws_route_table_association" "wp_private2_assoc" {
 
 
 
+# Security Group configuration
+
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "Used for the access to the dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+  #SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+
+#Public Security Group
+
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "Used for the ELB instance for public access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  #HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
 
 
 
 
+#Private Security Group
+
+
+resource "aws_security_group" "wp_private_sg" {
+  name        = "wp_private_sg"
+  description = "Used for private access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
 
 
 
 
+#RDS Security Group
+
+
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "Used for RDS access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+
+  #SQL access from public / private security groups
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+    "${aws_security_group.wp_private_sg.id}"]
+  }
 
 
 
-
-
-
-
-
-
-
+}
 
 
 
